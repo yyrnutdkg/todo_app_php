a9cba48b51ecd79c75d2b14b42ecf8d962d2930d
yyrnutdkg <yyrnutdkg@gmail.com>
2023-01-01 00:41:41 +0900

    [add]index雛形作成

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,9 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
+                <h5 class="card-header">
+                    <a href="{{ route('todo.create')}}" class="btn btn-outline-success">Todoを追加する！</a>
+                </h5>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -16,6 +19,18 @@
 
                     {{ __('You are logged in!') }}
                 </div>
+                <table>
+                    <thead>
+                        <th>Todoリスト</th>
+                    </thead>
+                    <tbody>
+                        <td>本を読む</td>
+                        <td>
+                            <a href="" class="btn btn-outline-primary">編集</a>
+                            <a href="" class="btn btn-outline-danger">削除</a>
+                        </td>
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
