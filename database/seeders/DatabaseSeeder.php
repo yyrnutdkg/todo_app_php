a06f906df2d495c6d6ba8d698c7c555e595fbaa1
yyrnutdkg <yyrnutdkg@gmail.com>
2023-01-03 15:36:36 +0900

    [add]seeds の生成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(UsersTableSeeder::class);
+        //$this->call(UsersTableSeeder::class);
+        DB::table('users')->delete();
+        DB::table('todos')->delete();
+        \App\Models\User::factory(5)->create();
+        \App\Models\Todo::factory(5)->create();
     }
 }
